cbf615440ee02b39e8013dea2609fbe2558791ad
metrospark <corporate@kedia.us>
2015-04-28 03:49:04 -0400

    ->Payshares

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
-/* exported STELLAR_CLIENT_REVISION */
+/* exported PAYSHARES_CLIENT_REVISION */
 /* jslint camelcase: false */
-var STELLAR_CLIENT_REVISION = '_GIT_REVISION_GOES_HERE_';
+var PAYSHARES_CLIENT_REVISION = '_GIT_REVISION_GOES_HERE_';
 
 console.log("%cStop!", "color:white; background:red; font-size: 16pt");
 console.log("%cThis is browser feature intended for developers. If someone told you to copy-paste something here, it is a scam and will give them access to your money!", "font-size: 14pt");
